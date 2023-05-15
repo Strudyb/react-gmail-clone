76eb6206ec3d14f1cd310f8e1628ed0d830f7ef9
Berk Yılmaz <ylmzbrk@outlook.com>
2023-05-16 00:28:17 +0300

    slice name

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import amk from '../features/mailSlice';
+import mailSlice from '../features/mailSlice';
 
 export const store = configureStore({
   reducer: {
-    mail: amk,
+    mail: mailSlice,
   },
 });
